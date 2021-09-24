290cc8882fd0024a35c30e6a1389c335d292fd5a
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-24 00:42:32 -0400

    updated file

---
+++ exercise24/src/main/java/Solution24.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+
+import java.util.Scanner;
+/*
+ *pseudocode
+ *       -prompt for the first string and store the value
+ *       -prompt for the second string and store the value
+ *       -run the two values through the anagram function
+ *           -print out if the two are an anagram
+ *
+ *   Anagram function
+ *       -take in the two strings to compare using the isAnagram function
+ *       -return if the two are an anagram or not
+ */
+//Create a program that compares two strings and determines if the two strings are anagrams.
+public class Solution24 {
+    //Initialize a scanner that will be used throughout the code
+    private final Scanner scanner = new Scanner(System.in);
+    //Print out a statement asking for two strings for the program to determine if they are an anagram
+    public static void main (String[] args) {
+
+    }
+
+}
