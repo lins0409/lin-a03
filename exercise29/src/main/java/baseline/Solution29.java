e9d7ff52077599fd31f6e1ba29d68ba907d660b3
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-27 00:50:57 -0400

    First commit

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+* The rule of 72 is a quick method for estimating how long it will take to double your investment, by taking the number 72 and dividing it by the expected rate of return. It’s a good tool that helps you figure out if the stock, bond, or savings account is right for you. It’s also a good program to write to test for and prevent bad input because computers can’t divide by zero. And instead of exiting the program when the user enters invalid input, you can just keep prompting for inputs until you get one that’s valid.
+*Write a quick calculator that prompts for the rate of return on an investment and calculates how many years it will take to double your investment.
+ */
+public class Solution29 {
+    //initialize scanner
+    public static void main (String [] args){
+        //prompt for the rate of return
+            //if the value is zero, say that's it not a valid input and prompt again
+            //if the value is non-numeric,  say that it is invalid and prompt again
+        //if the value is good call the print function
+    }
+    //print and calculate function
+    //pass in the rate of return
+    void printResults(int rate){
+        //years = 72/ rate
+        //print out the results of the equation
+    }
+}
