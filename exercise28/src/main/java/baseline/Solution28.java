fa29b60ce669ef7b54eae4b39b2b4527ec9124b9
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-27 00:33:00 -0400

    First commit

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+*In previous programs, you asked the user for repeated input by writing the input statements multiple times. But it’s more efficient to use loops to deal with repeated input.
+*Write a program that prompts the user for five numbers and computes the total of the numbers.
+ */
+public class Solution28 {
+    //initialize scanner
+    public static void main (String[] args){
+        //make an int total
+        //create a for loop that will repeat and ask for the value
+            //add the total each time
+        //call method that will print out the solution
+    }
+    //pass in the total
+    void printResults(){
+        //print out the solution
+    }
+}
