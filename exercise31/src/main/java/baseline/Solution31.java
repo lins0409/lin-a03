fcc9609e34c474a379b78b6fafb90c0cb035d482
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 00:23:31 -0400

    First commit

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+*/
+package baseline;
+import java.util.Scanner;
+
+/*
+*When you loop, you can control how much you increment the counter; you don’t always have to increment by one.
+*When getting into a fitness program, you may want to figure out your target heart rate so you don’t overexert yourself. The Karvonen heart rate formula is one method you can use to determine your rate. Create a program that prompts for your age and your resting heart rate. Use the Karvonen formula to determine the target heart rate based on a range of intensities from 55% to 95%. Generate a table with the results as shown in the example output.
+ */
+public class Solution31 {
+    //initialize scanner
+    public static void main (String[] args){
+        //prompt for age
+        //scan the age in and store the value
+        //prompt for their resting pulse
+        //make sure that if the entered result isn't a number don't continue
+        //scan the value and store it
+        //call the targetHeartRate function
+    }
+
+    //calculates and prints out the table
+    //pass in heart rate and age
+    void targetHeartRate(){
+        //print out resting heart rate and age in the same line
+        //calculate the target heart rate
+        //print out intensity and rate separated by lines
+        //make a loop to print out the percentages
+    }
+}
