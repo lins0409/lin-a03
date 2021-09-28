44525c1027187def12e40f57f9e510c1f2832363
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 19:11:04 -0400

    First commit

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+*Arrays don’t have to be hard-coded. You can take user input and store it in an array and then work with it.
+*
+*Create a program that picks a winner for a contest or prize drawing. Prompt for names of contestants until the user leaves the entry blank. Then randomly select a winner.
+ */
+public class Solution35 {
+    //initialize scanner and Solution35 caller
+    public static void main (String [] args){
+        //prompt user to enter a name
+        //loop it and scan the value, storing it in an array until the user enters no input
+        //call the randomizer
+    }
+
+    //pass in the names that were entered
+    void randomizer(){
+        //randomly choose a name at random
+        //print out who the winner is
+    }
+}
