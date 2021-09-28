84118fa3a56e4f7836638280fcf585b161187102
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 18:52:11 -0400

    Updated pseudocode and with program logic

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -5,7 +5,11 @@
  */
 
 package baseline;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /*
 *Sometimes you have to locate and remove an entry from a list based on some criteria. You may have a deck of cards and need to discard one so it’s no longer in play, or you may need to remove values from a list of valid inputs once they’ve been used. Storing the values in an array makes this process easier. Depending on your language, you may find it safer and more efficient to create a new array instead of modifying the existing one.
@@ -15,17 +19,36 @@ import java.util.Scanner;
 
 public class Solution34 {
     //initialize scanner
+    private static final Scanner scanner = new Scanner(System.in);
+    public static final Solution34 sol = new Solution34();
+
     public static void main (String[] args){
         //print out that there are however many employees
+        int employee = 5;
+        System.out.print("There are " + employee +" employees: \n");
         //create an array to store the employees
+        String[] names =new String[] {"John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"};
+        List<String> list = new ArrayList<String>(Arrays.asList(names));
         //print out the names in the array
+        Arrays.stream(names).forEach(System.out::println);
         //prompt user for the name they want to delete from the array
+        System.out.print("Enter an employee name to remove: ");
         //scan in the value and store it
+        String delete = scanner.nextLine();
         //call the delete function
+        sol.removeEmployee(list, delete);
+        //update the array to no longer have that employee
+        names = list.toArray(new String[0]);
+        //decrease employee count by one
+        employee--;
         //print out the list afterwards and the number of employees there are
+        System.out.print("There are " + employee +" employees: \n");
+        Arrays.stream(names).forEach(System.out::println);
     }
+
     //delete function
-    void removeEmployee(){
+    void removeEmployee(List list, String delete){
         //delete the employee name that was passed in
+        list.remove(delete);
     }
 }
