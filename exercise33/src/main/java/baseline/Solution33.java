5dc140db2a3944a90b787903c8ba578827dc6cbc
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 03:02:40 -0400

    Updated pseudocode and with program logic

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -6,6 +6,7 @@
 
 package baseline;
 import java.util.Scanner;
+import java.util.Random;
 
 /*
 *Arrays are great for storing possible responses from a program. If you combine that with a random number generator, you can pick a random entry from this list, which works great for games.
@@ -14,15 +15,31 @@ import java.util.Scanner;
  */
 public class Solution33 {
     //initialize scanner
+    private static final Scanner scanner = new Scanner(System.in);
+    public static final Solution33 sol = new Solution33();
+
     public static void main (String[] args){
         //prompt for the question
+        System.out.print("What's your question? \n");
+        System.out.print("> ");
         //scan for the input
-        //run the random response function
-    }
-    //function that generates a response for the user
-    void randomResponse(){
-        //create an array of responses
+        String input = scanner.next();
+
+        Random random = new Random();
         //generate a random number
+        int randomNumber = random.nextInt(input.length());
+        //run the random response function
+        sol.randomResponse(randomNumber);
+    }
+
+    //function that generates a response for the user
+    void randomResponse(int randomNumber){
+        //create an array of responses
+        String[] responses = {"Yes","No","Maybe","Ask again later."};
         //print out the response
+        String output = responses[randomNumber];
+
+        System.out.println();
+        System.out.println(output);
     }
 }
