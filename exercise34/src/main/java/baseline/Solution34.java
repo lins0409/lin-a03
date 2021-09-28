a58f8e6fe5c0b2f7de8800c3f85865aeea4cfbde
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 15:13:38 -0400

    First commit

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+*Sometimes you have to locate and remove an entry from a list based on some criteria. You may have a deck of cards and need to discard one so it’s no longer in play, or you may need to remove values from a list of valid inputs once they’ve been used. Storing the values in an array makes this process easier. Depending on your language, you may find it safer and more efficient to create a new array instead of modifying the existing one.
+*
+*Create a small program that contains a list of employee names. Print out the list of names when the program runs the first time. Prompt for an employee name and remove that specific name from the list of names. Display the remaining employees, each on its own line.
+ */
+
+public class Solution34 {
+    //initialize scanner
+    public static void main (String[] args){
+        //print out that there are however many employees
+        //create an array to store the employees
+        //print out the names in the array
+        //prompt user for the name they want to delete from the array
+        //scan in the value and store it
+        //call the delete function
+        //print out the list afterwards and the number of employees there are
+    }
+    //delete function
+    void removeEmployee(){
+        //delete the employee name that was passed in
+    }
+}
