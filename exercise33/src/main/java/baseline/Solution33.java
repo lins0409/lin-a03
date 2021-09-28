ea2f61f6ede8e8aebe519a6a228fc792926b8b8f
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 02:44:18 -0400

    First commit

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+*Arrays are great for storing possible responses from a program. If you combine that with a random number generator, you can pick a random entry from this list, which works great for games.
+*
+*Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe," or "Ask again later."
+ */
+public class Solution33 {
+    //initialize scanner
+    public static void main (String[] args){
+        //prompt for the question
+        //scan for the input
+        //run the random response function
+    }
+    //function that generates a response for the user
+    void randomResponse(){
+        //create an array of responses
+        //generate a random number
+        //print out the response
+    }
+}
