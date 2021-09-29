7323de2e0504754d39908ae60f035e408692c197
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 22:26:18 -0400

    First commit

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+*Write a program that prompts for response times from a website in milliseconds. It should keep asking for values until the user enters “done.”
+ */
+public class Solution36 {
+    //initialize scanner and Solution36 instance
+    public static void main (String[] args){
+        //create an array that stores the numbers
+        //prompt the user to input values continuously until they enter done
+        //call the functions average, max, min and std
+    }
+
+    //average function
+    double average(){
+        //add all of the values together and then divide it by how many values are in array
+    }
+
+    //max function
+    int max(){
+        //find what the largest number is
+    }
+
+    //min function
+    int min (){
+        //find what the smallest number is
+    }
+
+    //std function
+    double std(){
+        //calculate the standard deviation
+    }
+}
