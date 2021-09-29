8da80de9a6a963e41550d3ff12e425b5b2439cdb
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-29 01:20:12 -0400

    First commit

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+*Create a program that prompts for a list of numbers, separated by spaces. Have the program print out a new list containing only the even numbers.
+ */
+public class Solution38 {
+    //initialize the scanner and Solution38
+    public static void main (String[] args){
+        //create an array to store the numbers
+        //prompt the user for a list of numbers
+        //store the numbers in an array and split them via space
+        //call the returnEven function
+    }
+
+    //only returns even numbers of the array
+    void returnEven(){
+        //create a new array for the even numbers only
+        //loop for the number of elements in the array
+        //divide them by 2
+        //add the results to the new array
+        //print out the even values in the new array
+    }
+}
