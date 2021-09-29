0cc60275384e89ab300a51816c3d7adb42754b43
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-29 00:40:34 -0400

    First commit

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,30 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.Random;
+
+/*
+*Create a program that generates a secure password. Prompt the user for the minimum length, the number of special characters, and the number of numbers. Then generate a password for the user using those inputs.
+ */
+public class Solution37 {
+    //initialize the scanner, Solution37, and Random
+    public static void main (){
+        //make a string for the letters, special characters, and numbers
+        //prompt the user for the minimum length, store the value
+        //prompt for the number of special characters, store the value
+        //prompt for the number of numbers, store the value
+        //pass it into a random password generator
+    }
+    //random password generator
+    //pass in all the numbers for things
+    void randomPasswordMaker(){
+        //use the numbers to generate a random string
+        //print out the password
+    }
+}
