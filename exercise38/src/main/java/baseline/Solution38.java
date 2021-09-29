685d545f562ec2e27b61562f7bcd18de5de0b097
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-29 01:42:40 -0400

    Updated pseudocode and with program logic

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -5,26 +5,56 @@
  */
 
 package baseline;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
 *Create a program that prompts for a list of numbers, separated by spaces. Have the program print out a new list containing only the even numbers.
  */
 public class Solution38 {
-    //initialize the scanner and Solution38
+    //initialize the scanner
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main (String[] args){
-        //create an array to store the numbers
         //prompt the user for a list of numbers
+        System.out.print("Enter a list of numbers, separated by spaces: ");
+        String input = scanner.nextLine();
+        //create an array to store the numbers
         //store the numbers in an array and split them via space
+        String[] numbers = input.split(" ");
         //call the returnEven function
+        Integer[] array = filterEvenNumbers(numbers);
+
+        //print out the even values in the new array
+        System.out.print("The even numbers are " );
+        for ( int i = 0; i < array.length; i++){
+            if( i < array.length - 1){
+                System.out.print(array[i] + " ");
+            }
+            else {
+                System.out.print(array[i] + ".");
+            }
+        }
     }
 
     //only returns even numbers of the array
-    void returnEven(){
+     public static Integer[] filterEvenNumbers(String[] numbers){
         //create a new array for the even numbers only
+        ArrayList<Integer> evens = new ArrayList<>();
         //loop for the number of elements in the array
-        //divide them by 2
-        //add the results to the new array
-        //print out the even values in the new array
+        for (String number : numbers) {
+            int num = Integer.parseInt(number);
+
+            //divide them by 2
+            if ((num % 2) == 0) {
+                //add the results to the new array
+                evens.add(num);
+            }
+        }
+        //convert the list into an array for printing
+        Integer[] array = new Integer[evens.size()];
+        array = evens.toArray(array);
+
+        return array;
     }
 }
