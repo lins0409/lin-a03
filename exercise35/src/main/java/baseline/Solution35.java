de8f4310c6cfea682619c9f6af235d03f418032e
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-28 21:49:41 -0400

    Updated pseudocode and with program logic

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -5,6 +5,8 @@
  */
 
 package baseline;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -14,15 +16,31 @@ import java.util.Scanner;
  */
 public class Solution35 {
     //initialize scanner and Solution35 caller
+    private static final Scanner scanner = new Scanner(System.in);
+    public static final Random random = new Random();
+    public static final Solution35 sol = new Solution35();
+
     public static void main (String [] args){
+        //create an array to store all the names that will be entered
+        ArrayList<String> names = new ArrayList<String>();
+        String input;
         //prompt user to enter a name
         //loop it and scan the value, storing it in an array until the user enters no input
+        do {
+            System.out.print("Enter a name: ");
+            input = scanner.nextLine();
+            names.add(input);
+        } while (!input.isEmpty());
+        names.remove(names.size() - 1);
         //call the randomizer
+        sol.randomizer(names);
     }
 
     //pass in the names that were entered
-    void randomizer(){
+    void randomizer(ArrayList names){
         //randomly choose a name at random
+        String result = (String) names.get(random.nextInt(names.size()));
         //print out who the winner is
+        System.out.println("The winner is... " + result + ".");
     }
 }
