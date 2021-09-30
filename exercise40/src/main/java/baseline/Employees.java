51b7c58b1c8f104f1f2925cf3a0706c43da7ca93
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-30 03:19:02 -0400

    Updated pseudocode and with program logic

--- exercise40/src/main/java/baseline/Employees.java
+++ exercise40/src/main/java/baseline/Employees.java
@@ -1,18 +1,27 @@
 package baseline;
 
 import java.util.Comparator;
+import java.util.Map;
 
 //to make the list of employees like Solution 39
 public class Employees {
+
     //declare strings for the first name, last name, position, and separation date
+    public String firstName, lastName, position, separationDate;
 
-    //create the constructor to store all of the values
-    public Employees(){
-
+    //create a constructor to store all the employee information
+    public Employees(String firstName, String lastName, String position, String separationDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.separationDate = separationDate;
     }
 }
 
 //create the comparator to be used in main
 abstract class nameFinder implements Comparator<Employees>{
-
+    Map<Integer, Employees> base;
+    public int compare(Employees employeeOne, Employees employeeTwo) {
+        return employeeOne.lastName.compareTo(employeeTwo.lastName);
+    }
 }
--- exercise40/src/main/java/baseline/Solution40.java
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -5,12 +5,10 @@
  */
 
 package baseline;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /*
-*Given the following data set create a program that lets a user locate all records that match the search string by comparing the search string to the first or last name field.
+ *Given the following data set create a program that lets a user locate all records that match the search string by comparing the search string to the first or last name field.
  */
 public class Solution40 {
     //initialize scanner to be used for when the value is going to be read in
@@ -18,16 +16,38 @@ public class Solution40 {
 
     public static void main (String[] args){
         //create a list that will hold all the employee information that will be stored in array form
-        List <Employees> list = new ArrayList<>();
+        List<Employees> list = new ArrayList<>();
         //store all the values listed in the chart, like Solution39
+        list.add(new Employees("John", "Johnson", "Manager", "2016-12-31"));
+        list.add(new Employees("Tou", "Xiong", "Software Engineer", "2016-10-05"));
+        list.add(new Employees("Michaela", "Michaelson", "District Manager", "2015-12-19"));
+        list.add(new Employees("Jake", "Jacobson", "Programmer", " "));
+        list.add(new Employees("Jacquelyn", "Jackson", "DBA", " "));
+        list.add(new Employees("Sally", "Webber", "Web Developer", "2015-12-18"));
+
+        //compare all the last names to each other so the list is organized by last name
+        list.sort(new nameFinder() {
+            @Override
+            public int compare(Employees employeeOne, Employees employeeTwo) {
+                return super.compare(employeeOne, employeeTwo);
+            }
+        });
+
         //prompt user for a search string
+        System.out.print("Enter a search string: ");
         //store the value to be compared
-        //loop so that if the string read in is found to match even a little part of the first or last name of the employee then it will print it out
+        String search = scanner.nextLine();
+
         //printout the formatted list
-        System.out.print("Results:\n\n");
+        System.out.print("\nResults:\n\n");
         System.out.println("Name                | Position          | Separation Date");
         System.out.println("--------------------|-------------------|----------------");
+
+        //loop so that if the string read in is found to match even a little part of the first or last name of the employee then it will print it out
+        for(Employees employed: list){
+            if(employed.firstName.contains(search) || employed.lastName.contains(search)) {
+                System.out.format("%-9s %-10s| %-18s| %s\n", employed.firstName, employed.lastName, employed.position, employed.separationDate);
+            }
+        }
     }
-
-
 }
