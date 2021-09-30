693ed64c767f9d39469b9b6f6543254c5eed6108
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-29 21:19:58 -0400

    First commit

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sue Lin
+ *
+ */
+
+package baseline;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Comparator;
+
+/*
+*Given the following data set, create a program that sorts all employees by last name and prints them to the screen in a tabular format.
+ */
+public class Solution39 {
+    //initialize Solution39
+
+    //create a constructor to store all the employee information
+
+    public static void main (String[] args){
+        //create a list that is equal to all the items in the employee array
+        //call a function that will get the employee's first and last names
+        //then compare the employee's last name using Comparator, so they can be sorted
+        //pass it into the print function to be printed in the right formats
+    }
+
+    //set all the employees to the correct positions and separation dates
+    public void setEmployees(){
+
+    }
+
+    //formated print statment
+    public void printTable(){
+        //start with the header, then print the items separated by |
+    }
+}
